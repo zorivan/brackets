f1413697ad72cb1150b60d7965000dcc91fcfd9a
Ivan Zorin <bravedeveloper@gmail.com>
2023-02-27 21:53:20 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const bracketsMap = new Map(bracketsConfig);
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (bracketsMap.has(str[i])) {
+      // it's an opening bracket
+      stack.push(str[i]);
+    } else {
+      // it's a closing bracket
+      if (stack.length === 0) {
+        return false;
+      }
+      // check if the top of the stack is the closing bracket for current symbol
+      if (str[i] === bracketsMap.get(stack[stack.length - 1])) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
