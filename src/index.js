3a3478a4c6c339e1746ef6f5abd1ddafbef3a2ad
Ivan Zorin <bravedeveloper@gmail.com>
2023-02-27 22:10:24 +0400

    feat: add solution for special case

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ module.exports = function check(str, bracketsConfig) {
   const bracketsMap = new Map(bracketsConfig);
   let stack = [];
   for (let i = 0; i < str.length; i++) {
+    let topElement = stack[stack.length - 1];
     if (bracketsMap.has(str[i])) {
       // it's an opening bracket
-      stack.push(str[i]);
+      if (topElement === str[i] && str[i] === bracketsMap.get(str[i])) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
     } else {
       // it's a closing bracket
       if (stack.length === 0) {
         return false;
       }
       // check if the top of the stack is the closing bracket for current symbol
-      if (str[i] === bracketsMap.get(stack[stack.length - 1])) {
+      if (str[i] === bracketsMap.get(topElement)) {
         stack.pop();
       } else {
         return false;
       }
     }
   }
+
   return stack.length === 0;
 }
